92d90bfde8ed072d03150c525d279bbe4ccce257
woojin624 <624jang@gmail.com>
2020-10-06 01:47:07 +0900

    Updates

---
+++ precache-manifest.3a74712451629c8173432b545b19259b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09007f1261df15bcb24b632854eceba",
+    "url": "/react_basic_card_maker/index.html"
+  },
+  {
+    "revision": "dfdb4d00a8e3ba0b03fd",
+    "url": "/react_basic_card_maker/static/css/main.57d1c239.chunk.css"
+  },
+  {
+    "revision": "5af8b87d0ae96f26a094",
+    "url": "/react_basic_card_maker/static/js/2.38be14e4.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/react_basic_card_maker/static/js/2.38be14e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdb4d00a8e3ba0b03fd",
+    "url": "/react_basic_card_maker/static/js/main.24281aaf.chunk.js"
+  },
+  {
+    "revision": "69f19d6a6f652a129a04",
+    "url": "/react_basic_card_maker/static/js/runtime-main.eab74776.js"
+  }
+]);
